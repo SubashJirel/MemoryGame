d0685b0b1c80b5cffd12d03ae45db4f52a37d9bc
Subash Jirel <subashjrl@gmail.com>
2024-02-02 23:00:01 +0545

    created shuffle cards function and added images source array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import uniqid from 'uniqid';
+const weatherImages = [
+  { src: '/img/clear.png' },
+  { src: '/img/clouds.png' },
+  { src: '/img/drizzle.png' },
+  { src: '/img/mist.png' },
+  { src: '/img/rain.png' },
+  { src: '/img/snow.png' },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  function shuffleCards() {
+    const twelveImagesArray = [...weatherImages, ...weatherImages]
+      .sort(() => Math.random() - 0.5)
+      .map((val) => ({ ...val, id: uniqid() }));
+    setCards(twelveImagesArray);
+    setTurns(0);
+  }
+  console.log(cards, turns);
   return (
     <>
       <h1>Memory game</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </>
   );
 }
